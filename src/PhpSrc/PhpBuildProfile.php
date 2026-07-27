c06b6e0894655de0e7689a0105a1434415a15a65
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2026-07-28 01:25:53 +0700

    refactor: increased extension coverage

--- src/PhpSrc/PhpBuildProfile.php
+++ src/PhpSrc/PhpBuildProfile.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace InternalsCS\PhpSrc;
 
+use function array_filter;
+use function array_unique;
+use function array_values;
+use function getenv;
 use function implode;
+use function is_dir;
+use function is_file;
+use function is_string;
+use function mb_strtoupper;
 use function sha1;
+use function str_replace;
 
 final readonly class PhpBuildProfile
 {
@@ -13,30 +22,49 @@ final readonly class PhpBuildProfile
     public function configureArgs(): array
     {
         return [
-            '--disable-all',
             '--enable-debug',
-            '--enable-cgi',
-            '--enable-zend-test',
-            '--enable-tokenizer',
             '--enable-bcmath',
             '--enable-calendar',
-            '--with-libxml',
-            '--enable-dom',
-            '--enable-fileinfo',
+            $this->optionWithOptionalPrefix('--with-bz2', 'bzip2', ['include/bzlib.h']),
+            '--with-curl',
+            '--enable-dba',
+            '--enable-dl-test',
+            '--enable-exif',
+            '--with-ffi',
+            '--enable-ftp',
+            '--enable-gd',
+            '--with-external-gd',
+            $this->optionWithOptionalPrefix('--with-gettext', 'gettext', ['include/libintl.h']),
+            $this->optionWithOptionalPrefix('--with-gmp', 'gmp', ['include/gmp.h']),
             '--enable-intl',
-            '--with-ldap',
+            $this->optionWithOptionalPrefix('--with-ldap', 'openldap', ['include/ldap.h']),
+            '--enable-mbstring',
+            '--with-mysqli=mysqlnd',
+            '--enable-mysqlnd',
+            $this->optionWithOptionalPrefix('--with-unixODBC', 'unixodbc', ['include/sql.h']),
             '--enable-pcntl',
-            '--enable-posix',
-            '--enable-phar',
-            '--enable-session',
-            '--enable-simplexml',
-            '--enable-soap',
-            '--enable-xml',
-            '--enable-xmlreader',
-            '--enable-xmlwriter',
-            '--enable-pdo',
+            $this->optionWithOptionalPrefix('--with-pdo-dblib', 'freetds', ['include/sybdb.h']),
+            '--with-pdo-mysql=mysqlnd',
+            $this->pdoOdbcOption(),
+            $this->optionWithOptionalPrefix('--with-pdo-pgsql', 'libpq', ['include/libpq-fe.h', 'include/postgresql/libpq-fe.h']),
             '--with-pdo-sqlite',
+            $this->optionWithOptionalPrefix('--with-pgsql', 'libpq', ['include/libpq-fe.h', 'include/postgresql/libpq-fe.h']),
+            $this->optionWithOptionalPrefix('--with-readline', 'readline', ['include/readline/readline.h']),
+            '--enable-shmop',
+            '--enable-soap',
+            $this->optionWithOptionalPrefix('--with-snmp', 'net-snmp', ['include/net-snmp/net-snmp-config.h']),
+            '--enable-sockets',
             '--with-sqlite3',
+            '--with-sodium',
+            '--enable-sysvmsg',
+            '--enable-sysvsem',
+            '--enable-sysvshm',
+            $this->optionWithOptionalPrefix('--with-tidy', 'tidy-html5', ['include/tidy/tidy.h', 'include/tidy.h']),
+            '--enable-zend-test',
+            $this->optionWithOptionalPrefix('--with-zlib', 'zlib', ['include/zlib.h']),
+            '--with-openssl',
+            '--with-zip',
+            '--enable-opcache',
         ];
     }
 
@@ -44,12 +72,24 @@ final readonly class PhpBuildProfile
     public function pkgConfigPackages(): array
     {
         return [
+            'bzip2',
+            'libcurl',
+            'gdlib',
+            'gmp',
             'icu-io',
             'icu-i18n',
             'icu-uc',
             'ldap',
             'libxml-2.0',
+            'libzip',
+            'netsnmp',
+            'oniguruma',
+            'openssl',
+            'libpq',
+            'libsodium',
             'sqlite3',
+            'tidy',
+            'zlib',
         ];
     }
 
@@ -65,5 +105,69 @@ final readonly class PhpBuildProfile
     public function signature(): string
     {
         return sha1(implode("\n", $this->configureArgs()));
+    }
+
+    /** @param list<string> $markers */
+    private function optionWithOptionalPrefix(string $option, string $dependency, array $markers): string
+    {
+        $prefix = $this->dependencyPrefix($dependency, $markers);
+
+        return null === $prefix ? $option : $option . '=' . $prefix;
+    }
+
+    private function pdoOdbcOption(): string
+    {
+        $prefix = $this->dependencyPrefix('unixodbc', ['include/sql.h']);
+
+        return '--with-pdo-odbc=unixODBC,' . ($prefix ?? '/usr');
+    }
+
+    /** @param list<string> $markers */
+    private function dependencyPrefix(string $dependency, array $markers): ?string
+    {
+        $environmentPrefix = getenv('INTERNALS_CS_' . $this->environmentName($dependency) . '_PREFIX');
+
+        if (is_string($environmentPrefix) && is_dir($environmentPrefix)) {
+            return $environmentPrefix;
+        }
+
+        foreach ($this->prefixCandidates($dependency) as $prefix) {
+            foreach ($markers as $marker) {
+                if (is_file($prefix . '/' . $marker)) {
+                    return $prefix;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private function environmentName(string $dependency): string
+    {
+        return str_replace(['-', '.'], '_', mb_strtoupper($dependency));
+    }
+
+    /** @return list<string> */
+    private function prefixCandidates(string $dependency): array
+    {
+        $candidates = [];
+
+        foreach (['HOMEBREW_PREFIX', 'CONDA_PREFIX', 'PREFIX'] as $name) {
+            $prefix = getenv($name);
+
+            if (is_string($prefix) && '' !== $prefix) {
+                $candidates[] = $prefix . '/opt/' . $dependency;
+                $candidates[] = $prefix;
+            }
+        }
+
+        foreach (['/opt/homebrew', '/opt/local', '/usr/local', '/usr'] as $prefix) {
+            $candidates[] = $prefix . '/opt/' . $dependency;
+            $candidates[] = $prefix;
+        }
+
+        return array_filter($candidates, is_string(...))
+            |> array_unique(...)
+            |> array_values(...);
     }
 }
