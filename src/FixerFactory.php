9e11c33807ae541a09417b1cbe80789ebb5dd8cb
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2026-08-19 19:58:13 +0700

    fix: reduced memory usage for batched fixer runs

--- src/FixerFactory.php
+++ src/FixerFactory.php
@@ -5,16 +5,30 @@ declare(strict_types=1);
 namespace InternalsCS;
 
 use InternalsCS\Fixers\ExceptionOutput\ExceptionOutputFixer;
+use InternalsCS\Fixers\ExceptionOutput\Fixing\ExpectedOutputUpdater;
+use InternalsCS\Fixers\ExceptionOutput\Fixing\OutputRewriteImpact;
+use InternalsCS\Fixers\ExceptionOutput\Fixing\OutputRewritePlanner;
 
 final readonly class FixerFactory
 {
+    public function __construct(
+        private OutputRewritePlanner $exceptionOutputPlanner = new OutputRewritePlanner(),
+        private ExpectedOutputUpdater $expectedOutputUpdater = new ExpectedOutputUpdater(),
+        private OutputRewriteImpact $outputRewriteImpact = new OutputRewriteImpact(),
+    ) {}
+
     /** @param class-string<Fixer> $fixerClass */
     public function create(string $fixerClass, FixerRunOptions $options): Fixer
     {
-        if (ExceptionOutputFixer::class === $fixerClass) {
-            return new ExceptionOutputFixer(skipNormalizationOnly: $options->skipNormalizationOnly);
+        if (ExceptionOutputFixer::class !== $fixerClass) {
+            return new $fixerClass();
         }
 
-        return new $fixerClass();
+        return new ExceptionOutputFixer(
+            planner: $this->exceptionOutputPlanner,
+            expectedOutput: $this->expectedOutputUpdater,
+            impact: $this->outputRewriteImpact,
+            skipNormalizationOnly: $options->skipNormalizationOnly,
+        );
     }
 }
